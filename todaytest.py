6c27b9db65c79616381b1a2e03ec89dd7be1452a
jmockbee <mockbee1000@gmail.com>
2019-03-26 17:36:01 -0400

    finished exercise

--- todaytest.py
+++ todaytest.py
@@ -1,6 +1,24 @@
-# start with this
-hrs = input("Enter Hours:")
-pr = input("Enter Rate:")
-Pay = float(hrs)* float (pr)
-print("Pay:", Pay)
+# using part of the lsat exercise 
+jhr = input("Enter Hours:")
+jr = input("Enter Rate:")
+try:  
+    fh = float (jhr)
+    fr = float (jr) 
+except:
+    print ("Error, please enter numeric input")
+    quit ()
+
+   #print (fh,fr)
+
+if fh > 40 :
+    regpay = fh * fr
+    otpay  = (fh - 40.0) * (0.5 * fr)
+    totalpay = regpay + otpay
+else :
+
+    totalpay = fh * fr 
+
+
+print("Pay:", totalpay)
+
 
